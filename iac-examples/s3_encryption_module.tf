cc8bf2d5673800d5872ed42f11b2473ba6fc0df7
Vladimir Kozin <vladimir.kozin1@gmail.com>
2025-11-25 12:13:19 -0500

    Add documentation to s3_encryption_module.tf
    Added documentation comments to explain the purpose of the file and link to the detailed IaC repository.

--- iac-examples/s3_encryption_module.tf
+++ iac-examples/s3_encryption_module.tf
@@ -1,1 +1,5 @@
+# This file serves as an index to our comprehensive Infrastructure-as-Code (IaC) library.
+# The full, detailed IaC repository demonstrating the secure S3 bucket configuration is located here:
+# https://github.com/VladimirKozin/AWS-S3-Secure-Bucket-Configuration
 
+# We use IaC to ensure all remediation steps are automated, auditable, and repeatable.
